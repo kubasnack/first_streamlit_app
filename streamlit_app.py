5fbd15beca52bc6a386e8925864948c8842043ac
DataOverlord <pihalaczkiewicz@hotmail.com>
2023-03-01 19:37:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 #New Section to display fruityvice api response
-fruit_choice2 = streamlit.text_input('What fruit would you like to add?)
+fruit_choice2 = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding: ', fruit_choice2)
